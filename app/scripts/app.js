3842bb1559fefa939ec3f12d28e2cc72acd3bb8f
jartek <jartek.91@gmail.com>
2014-05-02 23:33:31 +0530

    Inital Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,7 @@
+/*jshint unused: vars */
+define(['angular', 'controllers/main']/*deps*/, function (angular, MainCtrl)/*invoke*/ {
+  'use strict';
+
+  return angular.module('frontendApp', ['frontendApp.controllers.MainCtrl',
+/*angJSDeps*/]);
+});
